d126c31090b9d2abb8fa49a1483b49dac827b67a
fatima <fatimaazzam1@gmail.com>
2023-02-22 17:11:20 +0200

    Hello World - C#

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var book = new Book("fatima");
+            book.AddGrade(89.1);
+            
+
+
+            var grades = new List<double>() {12.5, 13, 6.6, 9};
+            grades.Add(56.1);
+            
+            var result = 0.0;
+           
+            foreach(var number in grades)
+            {
+                result += number;
+            }
+
+            result /=  grades.Count;
+            Console.WriteLine($"The average is {result:N1}");
+               
+        }
+    }
+    
+}

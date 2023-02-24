40d4ee7923fbf3570c6acd69cae304e8e3bd95fb
fatima <fatimaazzam1@gmail.com>
2023-02-24 10:39:34 +0200

    refactor book class

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -13,8 +13,37 @@ namespace GradeBook
         {
             grades.Add(grade);
         }
+
+        public void CalcResult(){
+            
+            
+            var result = 0.0;
+            var highGrade = double.MinValue;
+            var lowestGrade = double.MaxValue;
+            
+            foreach(var number in grades)
+            {
+                
+                lowestGrade = Math.Min(number, lowestGrade);
+                highGrade = Math.Max(number, highGrade);
+                result += number;
+            }
+
+            result /=  grades.Count;
+            Console.WriteLine($"The average is {result:N1}");
+            Console.WriteLine($"The Hightest geade {highGrade}");
+            Console.WriteLine($"The lowest grade {lowestGrade}");
+        }
+
+        
+
         private List<double> grades;
         private string name;
+
+        
+
+        
+
     }
 
 }
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,23 +10,12 @@ namespace GradeBook
         {
             
             var book = new Book("fatima");
-            book.AddGrade(89.1);
-            
-
-
-            var grades = new List<double>() {12.5, 13, 6.6, 9};
-            grades.Add(56.1);
-            
-            var result = 0.0;
+            book.AddGrade(90.1);
+            book.AddGrade(97.1);
+            book.AddGrade(98.1);
+            book.CalcResult();
+          
            
-            foreach(var number in grades)
-            {
-                result += number;
-            }
-
-            result /=  grades.Count;
-            Console.WriteLine($"The average is {result:N1}");
-               
         }
     }
     

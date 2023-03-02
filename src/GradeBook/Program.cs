c404de717e5edaf393690c9e9fc18eea1849f30c
fatima <fatimaazzam1@gmail.com>
2023-03-02 16:48:01 +0200

    user input

--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -10,9 +10,22 @@ namespace GradeBook
         {
             
             var book = new Book("fatima");
-            book.AddGrade(90.1);
-            book.AddGrade(97.1);
-            book.AddGrade(98.1);
+           
+
+            while (true)
+            {
+                Console.WriteLine("Enter a grade or 'q'  to quit");
+                var input = Console.ReadLine();
+
+                if (input == "q")
+                {
+                    break;
+                }
+                var grade = double.Parse(input);
+
+                book.AddGrade(grade);
+            };
+
             var stats  = book.CalcResult();
             Console.WriteLine($"The average is:  {stats.Average:N1}");
             Console.WriteLine($"The Hightest geade {stats.High}");
